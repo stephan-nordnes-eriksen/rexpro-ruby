7413437c129735bf7ce82b3ad30afa95484fe7fa
Stephan Nordnes Eriksen <stephanruler@gmail.com>
2014-09-06 11:50:20 +0200

    Adding uuid solver

--- lib/rexpro/client.rb
+++ lib/rexpro/client.rb
@@ -3,73 +3,84 @@ require 'tcp_timeout'
 require_relative './session'
 
 module Rexpro
-  class Client
-    DEFAULT_HOST = 'localhost'
-    DEFAULT_PORT = 8184
+	class Client
+		@@responses = {}
 
-    attr_reader :host, :port, :socket
+		DEFAULT_HOST = 'localhost'
+		DEFAULT_PORT = 8184
 
-    def initialize(opts = {})
-      opts = opts.dup
-      @host = opts.delete(:host) || DEFAULT_HOST
-      @port = opts.delete(:port) || DEFAULT_PORT
+		attr_reader :host, :port, :socket
 
-      @request_opts = {}
-      [:graph_name, :graph_obj_name].each do |key|
-        value = opts.delete(key)
-        @request_opts[key] = value if value
-      end
+		def initialize(opts = {})
+			opts = opts.dup
+			@host = opts.delete(:host) || DEFAULT_HOST
+			@port = opts.delete(:port) || DEFAULT_PORT
 
-      @socket_opts = opts
-      reconnect
-    end
+			@request_opts = {}
+			[:graph_name, :graph_obj_name].each do |key|
+				value = opts.delete(key)
+				@request_opts[key] = value if value
+			end
 
-    def reconnect
-      @socket.close if @socket && !@socket.closed?
-      begin
-        @socket = TCPTimeout::TCPSocket.new(@host, @port, @socket_opts)
-        @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, true)
-      rescue TCPTimeout::SocketTimeout => ex
-        raise Rexpro::RexproException.new(ex)
-      end
-    end
+			@socket_opts = opts
+			reconnect
+		end
 
-    def request(req)
-      req.write_to(@socket)
+		def reconnect
+			@socket.close if @socket && !@socket.closed?
+			begin
+				@socket = TCPTimeout::TCPSocket.new(@host, @port, @socket_opts)
+				@socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, true)
+			rescue TCPTimeout::SocketTimeout => ex
+				raise Rexpro::RexproException.new(ex)
+			end
+		end
 
-      Rexpro::Message.read_from(@socket).tap do |resp|
-        if resp.request_uuid.bytes.to_a != req.request_uuid.bytes.to_a
-          @socket.close
-          raise Rexpro::RexproException,
-                "request uuid of response didn't match request"
-        end
+		def request(req)
+			req.write_to(@socket)
+			is_our_response = false
+			
+			while !is_our_response
+				if @@responses[resp.request_uuid.bytes.to_a]
+					if @@responses[resp.request_uuid.bytes.to_a].is_a? Rexpro::Message::Error
+						err_msg = resp.error_message
+						err_msg << " [flag=#{resp.flag}]" if resp.flag
+						raise Rexpro::RexproError.new(err_msg)
+					end
 
-        if resp.is_a? Rexpro::Message::Error
-          err_msg = resp.error_message
-          err_msg << " [flag=#{resp.flag}]" if resp.flag
-          raise Rexpro::RexproError.new(err_msg)
-        end
-      end
-    rescue TCPTimeout::SocketTimeout => ex
-      raise Rexpro::RexproException.new(ex)
-    rescue SystemCallError
-      # Lets not leave an open connection in a potentially bad state
-      @socket.close
-      raise
-    end
+					return @@responses.delete(resp.request_uuid.bytes.to_a)
+				end
 
-    def new_session(opts = {})
-      opts = @request_opts.merge(opts)
-      req = Rexpro::Message::SessionRequest.new(opts)
-      resp = request(req)
-      Rexpro::Session.new(self, resp.session_uuid, resp.languages)
-    end
+				respones = Rexpro::Message.read_from(@socket).tap do |resp|
+					if resp.request_uuid.bytes.to_a == req.request_uuid.bytes.to_a
+						is_our_response = true
+					end
+				end
+				if !is_our_response
+					@@responses[resp.request_uuid.bytes.to_a] = responses
+				end
+			end
 
-    def execute(script, opts = {})
-      opts = @request_opts.merge(opts)
-      opts[:script] = script
-      msg = Rexpro::Message::ScriptRequest.new(opts)
-      request(msg)
-    end
-  end
+			rescue TCPTimeout::SocketTimeout => ex
+				raise Rexpro::RexproException.new(ex)
+			rescue SystemCallError
+				# Lets not leave an open connection in a potentially bad state
+				@socket.close
+				raise
+		end
+
+		def new_session(opts = {})
+			opts = @request_opts.merge(opts)
+			req = Rexpro::Message::SessionRequest.new(opts)
+			resp = request(req)
+			Rexpro::Session.new(self, resp.session_uuid, resp.languages)
+		end
+
+		def execute(script, opts = {})
+			opts = @request_opts.merge(opts)
+			opts[:script] = script
+			msg = Rexpro::Message::ScriptRequest.new(opts)
+			request(msg)
+		end
+	end
 end
